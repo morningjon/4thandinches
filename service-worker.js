9686ee42d2110ca38ab5862e35bfb48fe2702abf
morningjon <51994398+morningjon@users.noreply.github.com>
2025-06-24 18:03:18 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,31 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('draft-kicker-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/main.js',
-        '/manifest.json',
-        '/cheer.mp3',
-        '/boo.mp3',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
-  );
+const CACHE_NAME = 'draft-kicker-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/main.js',
+    '/manifest.json',
+    '/cheer.mp3',
+    '/boo.mp3'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
