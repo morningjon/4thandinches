1e58dc3559f445b7df3b48952d89f40d0dc72a85
morningjon <51994398+morningjon@users.noreply.github.com>
2025-06-25 17:52:35 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,22 @@
 const CACHE_NAME = 'draft-kicker-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/main.js',
-    '/manifest.json',
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/main.js',
+  '/manifest.json',
     '/cheer.mp3',
     '/boo.mp3'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(res => res || fetch(evt.request))
+  );
 });
+
+
